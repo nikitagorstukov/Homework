5d6a8971624e993d9e8e06a306b215e2698ede1f
nikitagorstukov <98657153+nikitagorstukov@users.noreply.github.com>
2022-04-19 16:42:49 +0200

    Homework_1_JavaScript

--- HW_1_JS.js
+++ HW_1_JS.js
@@ -1,8 +1,8 @@
 let item_1 = 5;
-// console.log (item_1);
+console.log (item_1);
 
 let item_2 = 3;
-// console.log (item_2);
+console.log (item_2);
 
 let item_3 = item_1 + item_2;
 console.log (item_3);
